02bdd70f314a9b156bf62ffa9a8ad08b5b1f7cee
Snaye Sotashe <sinayesotatshe@gmail.com>
2022-07-17 14:33:59 +0200

    deployed

---
+++ build/precache-manifest.9ebd082e8cd8869c461180466a3bda6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70995fd31d121e11a49f49083ca52dc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0debb9190e041ff2d25e",
+    "url": "/static/css/main.b1edbfae.chunk.css"
+  },
+  {
+    "revision": "d4e805ee29fdde33ee3f",
+    "url": "/static/js/2.a9cc2935.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a9cc2935.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0debb9190e041ff2d25e",
+    "url": "/static/js/main.73ea6222.chunk.js"
+  },
+  {
+    "revision": "9b52b5b3e93288bb4a0d",
+    "url": "/static/js/runtime-main.c96821d3.js"
+  }
+]);
